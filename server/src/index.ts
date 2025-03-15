5cc79cf4db4bd9db044ff5b459a0437ef38f2cdf
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-03-15 06:56:54 +0530

    add updates and create skeleton for be and fe

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hi there!");
+import WebSocket from 'ws';
+
+const ws = new WebSocket('ws://localhost:8080');
+
+ws.on('error', console.error);
+
+ws.on('open', function open() {
+  ws.send('something');
+});
+
+ws.on('message', function message(data) {
+  console.log('received: %s', data);
+});
