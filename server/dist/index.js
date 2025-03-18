f435bcfe179ec591db396bf8d4caff692408520f
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-03-19 02:22:39 +0530

    fix cluttered ws

--- server/dist/index.js
+++ server/dist/index.js
@@ -2,17 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
-let userCount = 0;
 let allSockets = [];
 wss.on("connection", (socket) => {
     allSockets.push(socket);
-    userCount = userCount + 1;
-    console.log("User Connected #" + userCount);
+    console.log("User Connected #");
     socket.on("message", (message) => {
-        console.log('Message Received from user #' + userCount + ":: " + message.toString());
-        for (let i = 0; i < allSockets.length; i++) {
-            const s = allSockets[i];
-            s.send(message.toString() + ':: Sent from the server #' + userCount);
-        }
+        console.log('Message Received from user' + message.toString());
+        allSockets.forEach(s => {
+            console.log("message received", s.send(message.toString()));
+            s.send(message.toString());
+        });
     });
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,21 +2,23 @@ import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let userCount = 0;
 let allSockets = [];
 
 wss.on("connection", (socket) => {
   allSockets.push(socket);
 
-  userCount = userCount+1;
-  console.log("User Connected #"+userCount);
+  console.log("User Connected #");
 
-      socket.on("message", (message) => {
-        console.log('Message Received from user #'+userCount+":: "+message.toString());
-        for(let i=0; i<allSockets.length; i++) {
-          const s = allSockets[i];
-          s.send(message.toString()+':: Sent from the server #'+ userCount);
-        }
+    socket.on("message", (message) => {
+      console.log('Message Received from user'+message.toString());
+      
+      allSockets.forEach(s => {
+        console.log("message received", s.send(message.toString() ));
+        s.send(message.toString());
+      });
+    })
 
-      })
+
+
+      
 })
